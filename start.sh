5180f4c062272aa5c469056b17aa3c0d58c9e4c3
Nicholas Dinis <dinisusmc@aol.com>
2024-08-01 22:25:55 -0400

    set for cloud

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install python3 python3-pip git -y
-git clone https://github.com/dinisusmc/multi_service_automation.git
-cd multi_service_automation/
+git clone https://github.com/dinisusmc/docker_deploy.git
+cd docker_deploy/
 sudo pip3 install -r requirements.txt
-python3 backend.py
+sudo python3 backend.py
